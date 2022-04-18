1337cd053048876aee6d5adc95a472d52e00562a
jongwookWang <79205667+jongwookWang@users.noreply.github.com>
2022-04-18 14:07:58 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -7,20 +7,11 @@ class App{
         this.canvas = document.createElement('canvas');
         document.body.appendChild(this.canvas);
         this.ctx = this.canvas.getContext('2d');
-        
-        this.tmpCanvas = document.createElement('canvas');
-        this.tmpCtx = this.tmpCanvas.getContext('2d');
 
         this.pixelRatio = window.devicePixelRatio > 1 ? 2 :1;
-
-        this.ripple = new Ripple();
-
+        
         window.addEventListener('resize',this.resize.bind(this),false);
         this.resize();
-        
-        this.radius = 10;
-        this.pixelSize = 30;
-        this.dots = [];
 
         this.isLoaded = false;
         this.imgPos = {
@@ -36,10 +27,6 @@ class App{
             this.isLoaded = true;
             this.drawImage();
         };
-
-        window.requestAnimationFrame(this.animate.bind(this));
-
-        this.canvas.addEventListener('click', this.onClick.bind(this),false);
     }
 
     resize(){
@@ -49,12 +36,6 @@ class App{
         this.canvas.width = this.stageWidth * this.pixelRatio;
         this.canvas.height = this.stageHeight * this.pixelRatio;
         this.ctx.scale(this.pixelRatio, this.pixelRatio);
-
-        this.tmpCanvas.width = this.stageWidth;
-        this.tmpCanvas.height = this.stageHeight;
-
-        this.ripple.resize(this.stageWidth, this.stageHeight)
-
 
         if (this.isLoaded){
             this.drawImage();
@@ -92,89 +73,7 @@ class App{
             this.imgPos.width, this.imgPos.height,
         );
 
-        this.tmpCtx.drawImage(
-            this.image,
-            0,0,
-            this.image.width, this.image.height,
-            this.imgPos.x, this.imgPos.y,
-            this.imgPos.width, this.imgPos.height,
-        );
-        
-        this.imgDate = this.tmpCtx.getImageData(0,0,this.stageWidth,this.stageHeight);
-
-        this.drawDots() 
     }
-
-    drawDots() {
-        this.dots = [];
-
-        this.columns = Math.ceil(this.stageWidth / this.pixelSize);
-        this.rows = Math.ceil(this.stageHeight/this.pixelSize);
-
-        for(let i=0; i<this.rows; i++){
-            const y = (i+0.5) * this.pixelSize;
-            const pixelY = Math.max(Math.min(y,this.stageHeight),0);
-
-            for(let j=0; j<this.columns; j++){
-                const x = (j+0.5) * this.pixelSize;
-                const pixelX = Math.max(Math.min(x,this.stageWidth),0);
-            
-                const pixelIndex = (pixelX + pixelY * this.stageWidth) *4;
-                const red = this.imgDate.data[pixelIndex+0];
-                const green = this.imgDate.data[pixelIndex+1];
-                const blue = this.imgDate.data[pixelIndex+2];
-
-                const dot = new Dot(
-                    x,y,
-                    this.radius,
-                    this.pixelSize,
-                    red,green,blue,
-                );
-
-                this.dots.push(dot);
-
-                
-            }
-        }
-    }
-
-
-
-
-    animate(){
-        window.requestAnimationFrame(this.animate.bind(this));
-
-        this.ripple.animate();
-
-        for ( let i =0; i < this.dots.length; i++){
-            const dot = this.dots[i];
-            if(collide(
-                dot.x,dot.y,
-                this.ripple.x,this.ripple.y,
-                this.ripple.radius
-            )){
-                dot.animate(this.ctx);
-            }
-        }
-    }
-
-    onClick(e){
-        this.ctx.clearRect(0,0,this.stageWidth, this.stageHeight);
-
-        this.ctx.drawImage(
-            this.image,
-            0,0,
-            this.image.width, this.image.height,
-            this.imgPos.x, this.imgPos.y,
-            this.imgPos.width, this.imgPos.height,
-        );
-
-        this.ripple.start(e.offsetX, e.offsetY);
-    }
-
-
-
-
 }
 
 window.onload = () => {
